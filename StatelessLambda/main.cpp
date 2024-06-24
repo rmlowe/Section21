0ca6feb9755ca9fd2c789a3f9f91fc6ecf195b0a
Robert Lowe <robertmlowe@rmlowe.com>
2024-06-24 19:51:49 +0100

    270. Stateless Lambda Expressions Demo - Part 2

--- StatelessLambda/main.cpp
+++ StatelessLambda/main.cpp
@@ -23,13 +23,13 @@ public:
 };
 
 std::ostream &operator<<(std::ostream &os, const Person &rhs) {
-    os << "[Person: " << rhs.name << " : " << rhs.age << "]";
+    os << "[Person: " <<rhs.name << " : " <<rhs.age << "]";
     return os;
 }
 
 void test1() {
     std::cout << "\n---Test1 --------------------------" << std::endl;
-    
+
     [] () {std::cout << "Hi" << std::endl; }();
     
     [] (int x) { std::cout << x << std::endl;}(100);
@@ -37,16 +37,16 @@ void test1() {
     [](int x, int y) { std::cout << x+y << std::endl; }(100,200);
 }
 
-// Using values and references as lambda parameters
+// Using values and references as lambda parameters 
 void test2() {
     std::cout << "\n---Test2 --------------------------" << std::endl;
     
-    auto l1 = [] () {std::cout << "Hi" << std::endl;};
+    auto l1 =   [] () {std::cout << "Hi" << std::endl;};
     l1();
     
     int num1 {100};
     int num2 {100};
-    
+
     auto l2 = [](int x, int y) { std::cout << x+y << std::endl; };
     l2(10,20);
     l2(num1, num2);
@@ -68,19 +68,19 @@ void test3() {
     std::cout << stooge << std::endl;
     
     auto l4 = [] (Person p) {
-        std::cout << p << std::endl;
+        std::cout << p << std::endl; 
     };
     l4(stooge);
     
     auto l5 = [] (const Person &p) {
-        std::cout << p << std::endl;
+        std::cout << p << std::endl; 
     };
     l5(stooge);
     
     auto l6 = [] (Person &p) {
         p.set_name("Frank");
         p.set_age(25);
-        std::cout << p << std::endl;
+        std::cout << p << std::endl; 
     };
     l6(stooge);
     
@@ -90,35 +90,84 @@ void test3() {
 
 // used for test4
 // using std::function as a parameter in C++14 and greater
-// or
+// or 
 // auto as parameter type in C++20
-void filter_vector(const std::vector<int> &vec, std::function<bool(int)> func) {
-    
+void filter_vector(const std::vector<int> &vec, std::function<bool(int)> func)   {
+//void filter_vector (const std::vector<int>  &vec, auto func) {
+    std::cout <<"[ ";
+    for (int i: vec) {
+        if (func(i)) 
+            std::cout << i  << " ";
+    }
+    std::cout << "]" << std::endl;
 }
 
 // passing a lambda to a function
 void test4() {
+    std::cout << "\n---Test4 --------------------------" << std::endl;
+    std::vector<int> nums {10,20,30,40,50,60,70,80,90,100};
     
+    filter_vector(nums, [](int x) {return x>50;});
+    
+    filter_vector(nums, [](int x) {return x<=30;});
+    
+    filter_vector(nums, [](int x) {return x>= 30 && x <=60;});
 }
 
 // used for test5
 auto make_lambda() {
-    
+	return [] () {std::cout << "This lambda was made using the make_lambda function!" << std::endl;};
 }
 
 // returning a lambda from a function
 void test5() {
-    
+	std::cout << "\n---Test5 --------------------------" << std::endl;
+	
+	auto l5 = make_lambda();
+	
+	l5();
 }
 
 // using auto in the lambda parameter list
 void test6() {
+    std::cout << "\n---Test6 --------------------------" << std::endl;
+    auto l6 = [](auto x, auto y) {
+        std::cout << "x: " << x << " y: " << y << std::endl;
+    };
     
+    l6(10,20);
+    l6(100.3, 200);
+    l6(12.5, 15.54);
+    
+    l6 (Person("Larry", 18), Person("Curly", 22));
 }
 
 // Simple lambda examples with std::sort and std::for_each
 void test7() {
+      std::cout << "\n---Test7 --------------------------" << std::endl;
+
+    std::vector<Person> stooges {
+        {"Larry", 18},
+        {"Moe", 30},
+        {"Curly", 25}
+    };
+    std::sort(begin(stooges), end(stooges), [](const Person &p1, const Person &p2) {
+        return p1.get_name() < p2.get_name();
+    });
     
+    std::for_each(begin(stooges), end(stooges), [](const Person &p) {
+        std::cout << p << std::endl;
+    });
+    
+    std::cout << std::endl;
+    
+     std::sort(begin(stooges), end(stooges), [](const Person &p1, const Person &p2) {
+        return p1.get_age()< p2.get_age();
+    });
+    
+    std::for_each(begin(stooges), end(stooges), [](const Person &p) {
+        std::cout << p << std::endl;
+    });
 }
 
 int main()
@@ -129,8 +178,8 @@ int main()
     test4();
     test5();
     test6();
-    test7();
+	test7();
     
     std::cout << std::endl;
-    return 0;
+	return 0;
 }
